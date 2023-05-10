d65bd561a5134c51b06f9d29ed37ab11f2bdef52
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-05-10 12:33:54 -0500

    Construccion de las funciones admin

--- bank.js
+++ bank.js
@@ -93,19 +93,101 @@ while (!accessPassword) {
         }
 }
 
-
-
-
-
 //3. Solicitar la cantidad de billetes de 5,
 // 10, 20, 50 y 100 mil pesos COP.
 
+const bills = [
+    {   
+        valor: 100,
+        cantidad: 100,
+    },
+    {   
+        valor: 50 ,
+        cantidad: 100,
+    },
+    {   
+        valor: 20 ,
+        cantidad: 100,
+    },
+    {   
+        valor: 10,
+        cantidad: 100,
+    },
+    {   
+        valor: 5,
+        cantidad: 100,
+    },
+]
 
+const billchange = [ {   
+    valor: 100,
+    
+},
+{   
+},
+{   
+    valor: 20 ,
 
+},
+{   
+    valor: 10,
+
+},
+{   
+    valor: 5,
+},
+];
 
 // 4. Almacenar esta información en un array de objetos.
 // 5. Una vez tenga la información, debe mostrar en consola
 // la suma por cada denominación y el total general.
+let billinput = 0;
+let billoutput = 0 ;
+let totalbills = []
+let totalValor 
+let temporalbill = []
+
+
+
+if (accessPassword === true) {
+
+    if (users.type = 'admin') {
+        billchange[0].cantidad = Number(prompt('Ingrese la cantiddad de billetes de 100 a ingresar'))
+        billchange[1].cantidad = Number(prompt('Ingrese la cantiddad de billetes de 50 a ingresar'))
+        billchange[2].cantidad = Number(prompt('Ingrese la cantiddad de billetes de 20 a ingresar'))
+        billchange[3].cantidad = Number(prompt('Ingrese la cantiddad de billetes de 10 a ingresar'))
+        billchange[4].cantidad = Number(prompt('Ingrese la cantiddad de billetes de 5 a ingresar'))
+
+        for (let index = 0; index < bills.length; index++) {
+            bills[index].cantidad = bills[index].cantidad + billchange[index].cantidad
+            console.log(`La cantidad de billetes de ${bills[index].valor} es de ${bills.cantidad}`);
+        }
+
+        totalValor = bills.reduce((acc, curr) => acc + curr.valor, 0);
+        console.log("La cantidad total del dinero dentro del cajero es ", totalValor);
+    
+        access = false
+        accessPassword = false
+    }
+    else if (users.type = 'client') {
+
+            billoutput = Number(prompt('Ingresa la cantidad de dinero que deseas retirar'))
+            if ( billoutput < totalValor){
+                for (let i = 0; i < array.length; i++) {
+                   
+                    
+                }
+            }
+            else {console.log('Lo sentimos, actualmente no tenemos la cantidad de dinero solicitada.');
+            access = false
+            accessPassword = false
+        }
+
+
+    }
+}
+
+
 // 6. Una vez el cajero esté cargado, debe volver a
 // solicitar usuario y contraseña, si es administrador,
 // se repite el mismo proceso, sumar a la cantidad actual,
