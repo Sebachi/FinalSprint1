2a51558ef57c2ac02073c8c13b45348f1236ea1a
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-05-12 09:04:30 -0500

    Limpieza de codigo y construccion de comentarios

--- bank.js
+++ bank.js
@@ -1,8 +1,4 @@
-// 1. Escribir una lista de usuarios con los siguientes 
-// datos: nombre, número de documento, contraseña y tipo de usuario. 
-// El tipo de usuario será: 1: administrador, 
-// 2: cliente. Guardarla en un array de objetos.
-
+//Base de datos usuarios
 const users = [{
     username: 'armelio123',
     password: 13245,
@@ -41,26 +37,19 @@ const users = [{
     type: 'client',
 }
 ]
-
-
-
-// 2. Realizar un programa que al inicio solicite ingresar 
-// documento y contraseña, si el usuario no existe debe indicar 
-// que no existe y volver a preguntar usuario y contraseña, 
-// si el usuario es administrador, 
-// debe permitir cargar el cajero de la siguiente manera:
+//Declaracion de variables
 let access = false;
 let accessPassword = false
 let newUser;
 let newPassword;
 let currType = ''
+// Creacion de una funcion reseteo
 const reseteo = () => {
     access = false;
     accessPassword = false;
     currType = "";
     startProgram();
 };
-
 const bills = [
     {
         valor: 100,
@@ -83,11 +72,11 @@ const bills = [
         cantidad: 100,
     },
 ]
-
+// Inicio del programa
 const startProgram = () => {
+    //Validacion de usuario y contrasena  
     while (!access) {
         newUser = Number(prompt('Ingrese tu numero de documento: '));
-
         for (let index = 0; index < users.length; index++) {
             if (newUser === users[index].Id) {
                 access = true;
@@ -95,11 +84,9 @@ const startProgram = () => {
                 currType = users[index].type
                 break;
             }
-
         }
         if (!access) {
             console.log('El usuario no existe ');
-
         }
     }
     while (!accessPassword) {
@@ -111,15 +98,13 @@ const startProgram = () => {
                 break;
             }
         }
-
         if (!accessPassword) {
             console.log('Ingresaste mal la contrasena');
 
         }
     }
 
-    //3. Solicitar la cantidad de billetes de 5,
-    // 10, 20, 50 y 100 mil pesos COP.
+    // Declaracion de variables cache utilizadas para hacer operaciones
     const billchange = [{
         valor: 100,
         cantidad: 0,
@@ -141,109 +126,90 @@ const startProgram = () => {
         cantidad: 0,
     },
     ];
-
-    // 4. Almacenar esta información en un array de objetos.
-    // 5. Una vez tenga la información, debe mostrar en consola
-    // la suma por cada denominación y el total general.
     let billinput = 0;
     let billoutput = 0;
-     
-    const SumBillValue = (bills) => { 
+    let temporalbill = 0;
+    // Funcion para sumar todo el dinero en el cajero 
+    const SumBillValue = (bills) => {
         let totalValor = 0
         for (let index = 0; index < bills.length; index++) {
-        totalValor += (bills[index].valor * bills[index].cantidad)  } 
-        return totalValor}
-    let totalValor =  SumBillValue(bills)
-   
+            totalValor += (bills[index].valor * bills[index].cantidad)
+        }
+        return totalValor
+    }
+    let totalValor = SumBillValue(bills)
     console.log(`En el cajero hay ${totalValor}`);
-    let temporalbill = 0
-    // Funcion para redondear billetes
-    // let redondeo = () => {
-    //     let rounded = Math.floor(billoutput);
-    //     if (rounded % 5 !== 0) { rounded -= rounded % 5; }; return rounded
-    // }
+    //Segunda validacion de contrasena correcta
     if (accessPassword) {
-
+        // Operacion para ADMIN 
         if (currType == 'admin') {
             for (let i = 0; i < billchange.length; i++) {
                 billchange[i].cantidad = Number(prompt(`Ingrese la cantidad de billetes de ${bills[i].valor} a ingresar`))
-
             }
             for (let index = 0; index < bills.length; index++) {
-                bills[index].cantidad = bills[index].cantidad + billchange[index].cantidad
+                bills[index].cantidad += billchange[index].cantidad
                 console.log(`La cantidad de billetes de ${bills[index].valor} es de ${bills[index].cantidad}`);
             }
-           totalValor = SumBillValue(bills)
+            totalValor = SumBillValue(bills)
             console.log("La cantidad total del dinero dentro del cajero es " + totalValor);
+            //Reset del billchange
             for (let i = 0; i < billchange.length; i++) {
                 billchange[i].cantidad = 0
-            }         
+            }
         }
         // PARA LOS CLIENTES 
         else if (currType == 'client') {
-
+            //Si hay plata en el cajero
             if (totalValor != 0) {
                 billoutput = Number(prompt('Ingresa la cantidad de dinero que deseas retirar'))
                 billinput = billoutput
-                if (billoutput < totalValor) {
+                //Si hay suficiente dinero en el cajero
+                if (billoutput <= totalValor) {
                     console.log(`Ya ingrese el dinero`);
+                    // Conteo de billetes a entregar adecuada a la cifra solicitada
                     for (let i = 0; i < billchange.length; i++) {
                         while (billoutput >= bills[i].valor && bills[i].cantidad != 0) {
                             billoutput -= bills[i].valor;
                             bills[i].cantidad -= 1;
                             temporalbill += bills[i].valor;
-                            billchange[i].cantidad +=  1;
+                            billchange[i].cantidad += 1;
                         }
                     }
+                    // Si le entregara la cantidad total de la ingresada
                     if (temporalbill == billinput) {
                         console.log(`Se le puede entregar ${temporalbill}`);
                     }
+                    // Si solo se pudo entregar una determinada cifra debido al redondeo
                     else {
                         console.log(`Solo se pudo sacar ${temporalbill} de los ${billinput} solicitados`);
                     }
+                    //Avisa al usuario cuantos billetes de cada se entregaron
                     for (let index = 0; index < bills.length; index++) {
                         console.log(`La cantidad de billetes de ${billchange[index].valor} entregados es de ${billchange[index].cantidad}`);
                     }
+                    //Avisa al usuario cuantos billetes de cada se quedaron
                     for (let index = 0; index < bills.length; index++) {
                         console.log(`La cantidad de billetes de ${bills[index].valor} en el cajero es de ${bills[index].cantidad}`);
                         billchange[index].cantidad = 0
                     }
+                    //Se le resta al dinero total del cajero lo retirado y se muestra en consola
                     totalValor -= temporalbill
                     console.log("La cantidad total del dinero dentro del cajero es de " + totalValor);
-                  
                 }
+                //Si no hay suficiente dinero en el cajero
                 else {
                     console.log('Lo sentimos, actualmente no tenemos la cantidad de dinero solicitada.');
-                    
                 }
             }
+            // Si no hay plata en el cajero
             else {
                 console.log(`Cajero en mantenimiento, vuelva pronto.`);
-                
             }
         }
-       
     }
 }
+//Inicio del programa
 startProgram();
+//Boton para reset del programa
 const resetButton = document.getElementById('resetButton');
 resetButton.addEventListener('click', reseteo);
-
-
-// 6. Una vez el cajero esté cargado, debe volver a
-// solicitar usuario y contraseña, si es administrador,
-// se repite el mismo proceso, sumar a la cantidad actual,
-// si es cliente debe proseguir de la siguiente manera:
-// 7. Si el cajero no tiene dinero cargado, debe aparecer
-// un mensaje en consola: “Cajero en mantenimiento, vuelva
-// pronto.” Y reiniciar desde el inicio.
-// 8. Si el cajero ya tiene dinero cargado, debe preguntar
-// la cantidad deseada a retirar. Una vez obtenida la información,
-// debe indicar cuánto dinero puede entregar basado en la
-// cantidad disponible y los tipos de billetes. Luego debe
-// mostrar en consola cuántos billetes de cada denominación entregó.
-// Priorizando siempre las denominaciones más altas para valores altos
-// y redondeando a la cifra más cercana menor a la solicitada.
-// 9. Posteriormente, debe aparecer en consola, el dinero
-// restante en el cajero, por cada denominación.
-
